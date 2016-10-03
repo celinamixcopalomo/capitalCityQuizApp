24c54177e6db64b1396e76246e4c3d827f11f750
celinamixcopalomo <celinamixcopalomo@gmail.com>
2016-10-02 22:10:14 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,747 @@
+
+// the question object
+var questionObj = [
+{
+	question: "Afghanistan",
+	answer: "kabul",
+	asked: false
+},
+{
+	question: "Albania",
+	answer: "tirana",
+	asked: false
+},
+{
+	question: "Algeria",
+	answer: "algiers",
+	asked: false
+},
+{
+	question: "Andorra",
+	answer: "andora la vella",
+	asked: false
+},
+{
+	question: "Angola",
+	answer: "luanda",
+	asked: false
+},
+{
+	question: "Antigua and Barbuda",
+	answer: "saint john's",
+	asked: false
+},
+{
+	question: "Argentina",
+	answer: "buenos aires",
+	asked: false
+},
+{
+	question: "Armenia",
+	answer: "yerevan",
+	asked: false
+},
+{
+	question: "Austria" ,
+	answer: "vienna",
+	asked: false
+},
+{
+	question: "Azerbaijan" ,
+	answer: "baku",
+	asked: false
+},
+{
+	question: "Bahamas" ,
+	answer: "nassau",
+	asked: false
+},
+{
+	question: "Bahrain" ,
+	answer: "manama",
+	asked: false
+},
+
+{
+	question: "Bangladesh" ,
+	answer: "dhaka",
+	asked: false
+},
+{
+	question: "Barbados" ,
+	answer: "bridgetown",
+	asked: false
+},
+{
+	question: "Belarus" ,
+	answer: "minsk",
+	asked: false
+},
+{
+	question: "Belgium" ,
+	answer: "brussels",
+	asked: false
+},
+{
+	question: "Belize" ,
+	answer: "belmopan",
+	asked: false
+},
+{
+	question: "Benin" ,
+	answer: "porto novo",
+	asked: false
+},
+{
+	question: "Bolivia",
+	answer: "la paz",
+	asked: false
+},
+{
+	question: "Brazil" ,
+	answer: "brasilia",
+	asked: false
+},
+{
+	question: "Bulgaria" ,
+	answer: "sofia",
+	asked: false
+},
+{
+	question: "Canada" ,
+	answer: "ottawa",
+	asked: false
+},
+{
+	question: "Chile" ,
+	answer: "santiago",
+	asked: false
+},
+{
+	question: "China" ,
+	answer: "beijing",
+	asked: false
+},
+{
+	question: "Cuba" ,
+	answer: "havana",
+	asked: false
+},
+{
+	question: "Cyprus" ,
+	answer: "nicosia",
+	asked: false
+},
+{
+	question: "Czech Republic" ,
+	answer: "prague",
+	asked: false
+},
+{
+	question: "Denmark",
+	answer: "copenhagen",
+	asked: false
+},
+{
+	question: "Dominican Republic",
+	answer: "santo domingo",
+	asked: false
+},
+{
+	question: "Ecuador",
+	answer: "quito",
+	asked: false
+},
+{
+	question: "Egypt",
+	answer: "cairo",
+	asked: false
+},
+{
+	question: "El Salvador",
+	answer: "san salvador",
+	asked: false
+},
+{
+	question: "Estonia",
+	answer: "tallinn",
+	asked: false
+},
+{
+	question: "Ethiopia",
+	answer: "addis ababa",
+	asked: false
+},
+{
+	question: "Fiji",
+	answer: "suva",
+	asked: false
+},
+{
+	question: "Finland",
+	answer: "helsinki",
+	asked: false
+},
+{
+	question: "France",
+	answer: "paris",
+	asked: false
+},
+{
+	question: "Germany",
+	answer: "berlin",
+	asked: false
+},
+{
+	question: "Ghana",
+	answer: "accra",
+	asked: false
+},
+{
+	question: "Greece",
+	answer: "athens",
+	asked: false
+},
+{
+	question: "Guatemala",
+	answer: "guatemala",
+	asked: false
+},
+{
+	question: "Guyana",
+	answer: "georgetown",
+	asked: false
+},
+{
+	question: "Honduras",
+	answer: "tegucigalpa",
+	asked: false
+},
+{
+	question: "Hungary",
+	answer: "budapest",
+	asked: false
+},
+{
+	question: "Iceland",
+	answer: "reykjavik",
+	asked: false
+},
+{
+	question: "India",
+	answer: "new delhi",
+	asked: false
+},
+{
+	question: "Iran",
+	answer: "tehran",
+	asked: false
+},
+{
+	question: "Iraq",
+	answer: "baghdad",
+	asked: false
+},
+{
+	question: "Italy",
+	answer: "rome",
+	asked: false
+},
+{
+	question: "Jamaica",
+	answer: "kingston",
+	asked: false
+},
+{
+	question: "Japan",
+	answer: "tokyo",
+	asked: false
+},
+{
+	question: "Jordan",
+	answer: "amman",
+	asked: false
+},
+{
+	question: "Kenya",
+	answer: "nairobi",
+	asked: false
+},
+{
+	question: "Kuwait",
+	answer: "kuwait",
+	asked: false
+},
+{
+	question: "Latvia",
+	answer: "riga",
+	asked: false
+},
+{
+	question: "Liberia",
+	answer: "monrovia",
+	asked: false
+},
+{
+	question: "Libya",
+	answer: "tripoli",
+	asked: false
+},
+{
+	question: "Lithuania",
+	answer: "vilnius",
+	asked: false
+},
+{
+	question: "Luxembourg",
+	answer: "luxembourg",
+	asked: false
+},
+{
+	question: 'Macedonia',
+	answer: 'skopje',
+	asked: false
+},
+{
+	question: 'Madagascar',
+	answer:	'antananarivo',
+	asked: false
+},
+{
+	question: 'Malawi',
+	answer:	'lilongwe',
+	asked: false
+},
+{
+	question:'Malaysia',
+	answer: 'kuala Lumpur',
+	asked: false
+},
+{
+	question: 'Maldives',
+	answer: 'male',
+	asked: false
+},
+{
+	question: "Maldives",
+	answer: "male",
+	asked: false
+},
+{
+	question: 'Mali',
+	answer: 'bamako',
+	asked: false
+},
+{
+	question: 'Malta',
+	answer: 'valletta',
+	asked: false
+},
+{
+	question:'Marshall Islands',
+	answer:	'majuro',
+	asked: false
+},
+{
+	question: 'Mauritania',
+	answer: 'nouakchott',
+	asked: false
+},
+{
+	question: 'Mauritius',
+	answer:	'port louis',
+	asked: false
+},
+{
+	question: 'Mexico',
+	answer:	'mexico',
+	asked: false
+},
+{
+	question: 'Micronesia',
+	answer:	'palikir',
+	asked: false
+},
+{
+	question: 'Moldova',
+	answer:	'chisinau',
+	asked: false
+},
+{
+	question: 'Monaco',
+	answer: 'monaco',
+	asked: false
+},
+{
+	question: 'Mongolia',
+	answer:	'ulaanbaatar',
+	asked: false
+},
+{
+	question: 'Montenegro',
+	answer:	'podgorica',
+	asked: false
+},
+{
+	question: 'Morocco',
+	answer: 'rabat',
+	asked: false
+},
+{
+	question: 'Mozambique',
+	answer: 'maputo',
+	asked: false
+},
+{
+	question: 'Myanmar (Burma)',
+	answer: 'naypyidaw',
+	asked: false
+},
+{
+	question: "Nambia",
+	answer: "windhoek",
+	asked: false
+},
+{
+	question: "Nepal",
+	answer: "kathmandu",
+	asked: false
+},
+{
+	question: "Netherlands",
+	answer: "amsterdam",
+	asked: false
+},
+{
+	question: "New Zealand",
+	answer: "wellington",
+	asked: false
+},
+{
+	question: "Nicaragua",
+	answer: "managua",
+	asked: false
+},
+{
+	question: "Nigeria",
+	answer: "abuja",
+	asked: false
+},
+{
+	question: "North Korea",
+	answer: "pyongyang",
+	asked: false
+},
+{
+	question: "Norway",
+	answer: "oslo",
+	asked: false
+},
+{
+	question: "Oman",
+	answer: "muscat",
+	asked: false
+},
+{
+	question: "Pakistan",
+	answer: "islambad",
+	asked: false
+},
+{
+	question: "Panama",
+	answer: "panama",
+	asked: false
+},
+{
+	question: "Paraguay",
+	answer: "asuncion",
+	asked: false
+},
+{
+	question: "Peru",
+	answer: "lima",
+	asked: false
+},
+{
+	question: "Poland",
+	answer: "warsaw",
+	asked: false
+},
+{
+	question: "Portugal",
+	answer: "lisbon",
+	asked: false
+},
+{
+	question: "Qatar",
+	answer: "doha",
+	asked: false
+},
+{
+	question: "Romania",
+	answer: "bucharest",
+	asked: false
+},
+{
+	question: "Russia",
+	answer: "moscow",
+	asked: false
+},
+{
+	question: "South Korea",
+	answer: "seoul",
+	asked: false
+},
+{
+	question: "Saint Lucia",
+	answer: "castries",
+	asked: false
+},
+{
+	question: "Saint Vincent and the Genadines",
+	answer: "kingstown",
+	asked: false
+},
+{
+	question: "Saudi Arabia",
+	answer: "riyadh",
+	asked: false
+},
+{
+	question: "Senegal",
+	answer: "Dakar",
+	asked: false
+},
+{
+	question: "Serbia",
+	answer: "Belgrade",
+	asked: false
+},
+{
+	question: "Sierra Leone",
+	answer: "freetown",
+	asked: false
+},
+{
+	question: "Singapore",
+	answer: "singapore",
+	asked: false
+},
+{
+	question: "Slovakia",
+	answer: "ljubljana",
+	asked: false
+},
+{
+	question: "South Africa",
+	answer: "cape town",
+	asked: false
+},
+{
+	question: "South Korea",
+	answer: "seoul",
+	asked: false
+},
+{
+	question: "Spain",
+	answer: "madrid",
+	asked: false
+},
+{
+	question: "Suriname",
+	answer: "paramaribo",
+	asked: false
+},
+{
+	question: "Sweden",
+	answer: "stockholm",
+	asked: false
+},
+{
+	question: "Switzerland",
+	answer: "bern",
+	asked: false
+},
+{
+	question: "Taiwan",
+	answer: "taipei",
+	asked: false
+},
+{
+	question: "Thailand",
+	answer: "Bangkok",
+	asked: false
+},
+{
+	question: "Turkey",
+	answer: "ankara",
+	asked: false
+},
+{
+	question: "United Arab Emirates",
+	answer: "abu dhabi",
+	asked: false
+},
+{
+	question: "United Kingdom",
+	answer: "london",
+	asked: false
+},
+{
+	question: "United States of America",
+	answer: "washington d.c.",
+	asked: false
+},
+{
+	question: "Uruguay",
+	answer: "montevideo",
+	asked: false
+},
+{
+	question: "Venezuela",
+	answer: "caracas",
+	asked: false
+},
+{
+	question: "Vietnam",
+	answer: "hanoi",
+	asked: false
+},
+{
+	question: "Yemen",
+	answer: "sana'a",
+	asked: false
+},
+{
+	question: "Zambia",
+	answer: "lusaka",
+	asked: false
+},
+{
+	question: "Zimbabwe",
+	answer: "Harare",
+	asked: false
+},
+
+]
+
+// maximum of 8 question per document
+var maxQues = 8
+
+var rand = Math.floor(Math.random() * maxQues)
+
+	 for(var i=0;i<maxQues;i++){
+	 	//sitting the random number
+	 	 var rand = Math.floor(Math.random() * questionObj.length)
+
+	 	 //an if statment to insure no repetation
+	 	 if(questionObj[rand].asked == true) {
+	 	 	var rand = Math.floor(Math.random() * questionObj.length)
+	 	 }
+	 	// reasign the number "very important" for the answer to match
+	 	questionObj[i] = questionObj[rand]
+	 	// the question content
+	 	 q = "The capital city of " + questionObj[i].question + " ?"
+	 	 // the answer of the question
+	 	 var answer = questionObj[i].answer
+	 	 //sending it to the documet
+		 document.getElementById('question' + [i]).textContent = q
+		 //test on the console
+	 	 console.log(questionObj[i].question + answer + rand)
+	 	 //changing the status of the obj for the if statment to work.
+	 	 questionObj[i].asked = true
+
+	 }
+
+
+function calculateAnswer() {
+	//the counting of correct, incorrect and empty
+	var correct = 0
+	var incorrect = 0
+	var noAnswer = 0 
+
+
+
+	for (var i = 0; i < maxQues; i++) {
+		//setting the object answer
+		var answer = questionObj[i].answer
+		//getting the user's answer
+		userAnswer = document.getElementById('answer' + [i]).value.toLowerCase()
+
+		if (userAnswer == answer) {
+			// add the correct class
+			document.getElementById('question' + [i]).setAttribute('class', 'correct')
+			correct++
+			
+		} else if (userAnswer == "") {
+			// add the empty class
+			document.getElementById('question' + [i]).setAttribute('class', 'empty')
+			noAnswer++
+
+		} 
+		else {
+			// add the incorrect class
+			document.getElementById('question' + [i]).setAttribute('class', 'incorrect')
+			incorrect++
+		}	
+	}
+
+	 
+
+	// setting the result holder
+	var resultCorrect = document.getElementById('resultCorrect')
+	var resultIncorrect = document.getElementById('resultIncorrect')
+	var resultNoAnswer= document.getElementById('resultNoAnswer')
+
+	//creating h2 for the results of correct, incorrect and empty	
+	correctResult = document.createElement('h2')
+	incorrectResult = document.createElement('h2')
+	noAnswerResult = document.createElement('h2')
+
+	//adding class conected to CSS to change the style of each
+	correctResult.className = "answer-correct"
+	incorrectResult.className = "answer-incorrect"
+	noAnswerResult.className = "answer-didntAnswer"
+
+	//attaching the text and the counting to the output
+	correctResult.textContent = 'CORRECT: ' + correct
+	incorrectResult.textContent = 'INCORRECT: ' + incorrect
+	noAnswerResult.textContent = "DIDN'T Answer: " + noAnswer
+
+	//appending the h2s to the output div
+	resultCorrect.appendChild(correctResult)
+	resultIncorrect.appendChild(incorrectResult)
+	resultNoAnswer.appendChild(noAnswerResult)
+
+//===============	//aditional result =====================//
+	// getting the output
+	var sideOutput = document.getElementById('resultSide')
+	// creating p element
+	var statement = document.createElement('p')
+
+	sideOutput.appendChild(statement)
+
+	// assifning the messages 
+	var magnificent = "magnificent: you scored " + correct + " / " + maxQues
+	var veryWell = "VERY-WELL: you scored " + correct + " / " + maxQues
+	var halfWay = "HALFWAY: you scored " + correct + " / " + maxQues
+	var fail = "YOU REALLY NEED TO STUDY THIS THING... you scored " + correct + " / " + maxQues
+
+	// the if condition
+	if ( correct == maxQues )  {
+		statement.textContent = magnificent
+	}
+
+	else if (correct >= (maxQues * 80) /100) {
+		statement.textContent = veryWell
+	}
+
+	else if (correct >= (maxQues * 50) /100) {
+		statement.textContent = halfWay
+	}
+
+	else if (correct < maxQues / 2 ) {
+		statement.textContent = fail
+	}
+
+}
